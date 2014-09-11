28b15e03ed96f5514c299cb200033e23deba349c
= <cc2681a@student.american.edu>
2014-09-10 23:35:49 -0400

    Docs: Applied License evenly

--- src/main/scala/com/celexus/conniption/model/Quote.scala
+++ src/main/scala/com/celexus/conniption/model/Quote.scala
@@ -1,11 +1,23 @@
+/**
+ * Copyright 2014 Cameron Cook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.celexus.conniption.model
 
 import scala.xml.NodeSeq
 import java.text.{SimpleDateFormat, DateFormat}
 
-/**
- * Created by cam on 3/16/14.
- */
 class Quote(xml: NodeSeq, res: org.scribe.model.Response) extends TKResponse(xml: NodeSeq, res: org.scribe.model.Response, format = "xml") {
 
   def avg100Day: Double = toDouble(ends("quote/adp_100"))
