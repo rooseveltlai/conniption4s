d28041cb1857e0c2d03db462abcdc1f998d871a9
roosevelt lai <roosevelt.lai@gmail.com>
2017-11-05 19:04:00 -0500

    fix account issue

--- src/main/scala/com/celexus/conniption/model/Account.scala
+++ src/main/scala/com/celexus/conniption/model/Account.scala
@@ -37,7 +37,7 @@ class Account(xml: NodeSeq, res: org.scribe.model.Response) extends TKResponse(x
    * @return A BuyingPower object which contains what types of usuable funds there are
    */
   def buyingPower: BuyingPower = {
-    val buyingPowerXML: NodeSeq = (xml \ "accountbalance" \ "buyingpower")
+    val buyingPowerXML: NodeSeq = (xml \ "buyingpower")
     new BuyingPower(buyingPowerXML,res)
   }
 
@@ -57,7 +57,7 @@ class Account(xml: NodeSeq, res: org.scribe.model.Response) extends TKResponse(x
    * @return A funds object which returns account balances, currently accessible or not
    */
   def funds: Funds = {
-    val moneyXML: NodeSeq = (xml \ "accountbalance" \ "money")
+    val moneyXML: NodeSeq = (xml \ "money")
     new Funds(moneyXML,res)
   }
 
@@ -65,7 +65,7 @@ class Account(xml: NodeSeq, res: org.scribe.model.Response) extends TKResponse(x
    * @return A securities object which generalizes info about all the Securities held
    */
   def securities: Securities = {
-    val securitiesXML: NodeSeq = (xml \ "accountbalance" \ "securities")
+    val securitiesXML: NodeSeq = (xml \ "securities")
     new Securities(securitiesXML,res)
   }
 
@@ -73,7 +73,7 @@ class Account(xml: NodeSeq, res: org.scribe.model.Response) extends TKResponse(x
    * @return A Set of Holdings object to describe holdings the account has
    */
   def holdings: Set[Holding] = {
-    val holdingsXML: NodeSeq = xml \ "accountbalance" \ "accountholdings" \ "holding"
+    val holdingsXML: NodeSeq = xml \ "accountholdings" \ "holding"
     var holds: Set[Holding] = Set()
     holdingsXML.foreach {
       n: NodeSeq => holds += new Holding(n,res)
